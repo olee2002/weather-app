8fea4be5a32c9cc522f6315c0051811dd8d44da6
babylee2002 <babylee2002@gmail.com>
2020-09-29 00:55:36 -0400

    clean up

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from "redux";
-import appReducer from "./reducer";
+import { combineReducers } from 'redux'
+import appReducer from './reducer'
 
 const rootReducer = combineReducers({
-  appReducer
-});
+   appReducer,
+})
 
-export default rootReducer;
+export default rootReducer
