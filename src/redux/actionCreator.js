d917a5d93d6187c548668932db7199b08b1988c9
babylee2002 <babylee2002@gmail.com>
2020-09-29 01:06:27 -0400

    updating

--- src/redux/actionCreator.js
+++ src/redux/actionCreator.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 import { FETCH_FORECAST_DATA, FETCH_CURRENT_DATA } from './actionTypes'
 
-const { REACT_APP_WEATHER_API_HOST, REACT_APP_WEATHER_API_KEY } = process.env
+const { REACT_APP_WEATHER_API_KEY } = process.env
+const REACT_APP_WEATHER_API_HOST='https://api.openweathermap.org/data/2.5/'
 
 // creating an action creator for an async action and dispatch it to the store (Thunk middleware enables the dispatch)
 export const getWeatherForecast = (city) => (dispatch) => {
